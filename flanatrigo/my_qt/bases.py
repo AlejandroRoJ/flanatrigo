b0003a218bde57bbc537149674261ba2bf2e1181
AlberLC <alberlc@outlook.com>
2023-09-12 03:43:59 +0200

    Update spin boxes

--- flanatrigo/my_qt/bases.py
+++ flanatrigo/my_qt/bases.py
@@ -1,20 +1,34 @@
 from abc import ABC, ABCMeta
 
-from PySide6 import QtCore
+from PySide6 import QtCore, QtGui, QtWidgets
 
 
 class MixinMeta(type(QtCore.QObject), ABCMeta):
     pass
 
 
-# noinspection PyUnresolvedReferences
+# noinspection PyPep8Naming, PyUnresolvedReferences
 class NoWheel(ABC, metaclass=MixinMeta):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setFocusPolicy(QtCore.Qt.FocusPolicy.StrongFocus)
+        self.setCorrectionMode(QtWidgets.QSpinBox.CorrectToNearestValue)
 
-    def event(self, event: QtCore.QEvent) -> bool:
-        if event.type() == QtCore.QEvent.Wheel:
-            return False
+    def keyPressEvent(self, event: QtGui.QKeyEvent):
+        if event.key() == QtCore.Qt.Key_Backspace:
+            suffix = self.suffix()
+            if suffix:
+                edit = self.lineEdit()
+                text = edit.text()
+                if text.endswith(suffix) and text != self.specialValueText():
+                    pos = edit.cursorPosition()
+                    end = len(text) - len(suffix)
+                    if pos > end:
+                        edit.setCursorPosition(end)
+        super().keyPressEvent(event)
 
-        return super().event(event)
+    def wheelEvent(self, event: QtGui.QWheelEvent):
+        if self.hasFocus():
+            return super().wheelEvent(event)
+        else:
+            event.ignore()
--- flanatrigo/my_qt/spin_boxes.py
+++ flanatrigo/my_qt/spin_boxes.py
@@ -1,4 +1,4 @@
-from PySide6 import QtWidgets
+from PySide6 import QtCore, QtGui, QtWidgets
 
 from my_qt.bases import NoWheel
 
@@ -8,4 +8,13 @@ class NoWheelSpinBox(NoWheel, QtWidgets.QSpinBox):
 
 
 class NoWheelDoubleSpinBox(NoWheel, QtWidgets.QDoubleSpinBox):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setLocale(QtCore.QLocale.Language.English)
+
+    def keyPressEvent(self, event: QtGui.QKeyEvent):
+        if event.key() in (QtCore.Qt.Key_Period, QtCore.Qt.Key_Comma):
+            cursor_position = self.lineEdit().cursorPosition()
+            text_before_cursor = self.lineEdit().text()[:cursor_position].replace('.', '').replace(',', '.')
+            self.lineEdit().setText(f'{text_before_cursor}.')
+        super().keyPressEvent(event)
