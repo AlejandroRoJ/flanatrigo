83dd9873f3997f638bccdccc90d0964e7f838a21
AlberLC <alberlc@outlook.com>
2023-09-13 14:19:05 +0200

    Fix ImageRotatingFileHandler.doRollover

--- flanatrigo/models/logging_handlers.py
+++ flanatrigo/models/logging_handlers.py
@@ -10,14 +10,16 @@ class ImageRotatingFileHandler(logging.handlers.RotatingFileHandler):
     def doRollover(self):
         super().doRollover()
         path = list(pathlib.Path(constants.LOGS_PATH).glob(f"{constants.LOG_FILE_STEM}*"))[-1]
-        if not (lines := path.read_text().strip().splitlines()):
+        try:
+            line = path.read_text().strip().splitlines()[0]
+        except IndexError:
             return
 
         try:
-            date_string = re.findall(r'\d{2}/\d{2}/\d{4} - \d{2}:\d{2}:\d{2}', lines[-1])[0]
-            last_date = datetime.datetime.strptime(date_string, '%m/%d/%Y %H:%M:%S')
+            date_string = re.findall(r'\d{2}/\d{2}/\d{4} - \d{2}:\d{2}:\d{2}', line)[0]
+            last_date = datetime.datetime.strptime(date_string, '%m/%d/%Y - %H:%M:%S')
         except IndexError:
-            date_string = re.findall(r'\d{2}-\d{2}-\d{4}_\d{2}-\d{2}-\d{2}', lines[-1])[0]
+            date_string = re.findall(r'\d{2}-\d{2}-\d{4}_\d{2}-\d{2}-\d{2}', line)[0]
             last_date = datetime.datetime.strptime(date_string, '%m-%d-%Y_%H-%M-%S')
 
         for path in pathlib.Path(constants.LOGS_IMAGES_PATH).iterdir():
